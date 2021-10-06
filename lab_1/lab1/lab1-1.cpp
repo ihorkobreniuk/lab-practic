7fd16a449fc618f90d75e1d1b0189265d7d537e2
ihorkobreniuk <91456459+ihorkobreniuk@users.noreply.github.com>
2021-10-06 09:47:43 +0300

    Add files via upload

---
+++ lab_1/lab1/lab1-1.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f;
+
+	a = sizeof(int);
+	b = sizeof(short);
+	c = sizeof(long);
+	d = sizeof(char);
+	e = sizeof(float);
+	f = sizeof(double);
+
+
+	
+	
+
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
---
+++ lab_1/lab1/lab1-6.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+
+	a = 60;
+	b = 24;
+	c = 365;
+	d = a * a * b * c;
+
+
+
+
+
+
+	return 0;
+}
