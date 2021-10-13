be8944e32f7628d69ce776171162e72307e10022
ihorkobreniuk <91456459+ihorkobreniuk@users.noreply.github.com>
2021-10-13 10:04:20 +0300

    Add files via upload

---
+++ Lab_1/Lab1-1/Lab1-1.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f;
+
+	a = sizeof(int);
+	b = sizeof(short);
+	c = sizeof(long);
+	d = sizeof(char);
+	e = sizeof(float);
+	f = sizeof(double);
+
+
+
+	return 0;
+}
---
+++ Lab_1/lab1-6/lab1-6.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long Current;
+	long long s = 60 * 60 * 24 * 365;
+	long long s100 = s * 100;
+	long long s1000 = s * 1000;
+	Current = 2021 * s;
+
+
+	return 0;
+}
+
